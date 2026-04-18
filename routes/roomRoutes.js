f222f09f8e6c93bf3226b22b3c9751c91e8c79a0
Telbo <kamelabdelrahman73@gmail.com>
2026-04-18 13:31:27 +0200

    --

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -10,7 +10,7 @@ const { authMiddleware } = require("../middleWares/authMiddleWare");
 // routes
 router.use(authMiddleware);
 // Create Room Route
-router.post("/", allowTo("admin", "receptionist"),roomController.createNewRoom);
+router.post("/", allowTo("admin"),roomController.createNewRoom);
 // Get Rooms Route
 router.get("/", allowTo("admin", "receptionist"), roomController.findAllRooms);
 // Get Room Route
@@ -18,7 +18,7 @@ router.get("/:id", allowTo("admin", "receptionist"), roomController.findRoomById
 // Update Room Route
 router.put("/:id", allowTo("admin", "receptionist"), roomController.updateRoomById);
 // Delete Room Route
-router.delete("/:id", allowTo("admin", "receptionist"), roomController.deleteRoomById);
+router.delete("/:id", allowTo("admin"), roomController.deleteRoomById);
 
 //export router
 module.exports = router
